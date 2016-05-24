be55a58966e087884e8af6682115e5ce75cd03fb
Megan Ford <meg.c.ford@gmail.com>
2016-05-24 15:58:12 -0500

    exercise 2 completed

---
+++ songs.js
@@ -0,0 +1,35 @@
+var songs = [];
+
+songs.push({
+  id: songs.length+1,
+  artist: "Tzchaikovsky", 
+  song: "Overture",
+  album: "Swan Lake", 
+  genre: "Ballet"
+})
+
+songs.push({
+  id: songs.length+1,
+  artist: "Beethoven", 
+  song: "Allegro", 
+  album: "Op. No. 6", 
+  genre: "Classical"
+})
+
+songs.push({
+  id: songs.length+1,
+  artist: "Bach", 
+  song: "Fugue no.42 in C# Major", 
+  album: "Fugues and Preludes", 
+  genre: "Baroque"
+})
+
+songs.push({
+  id: songs.length+1,
+  artist: "Mozart", 
+  song: "Popagena", 
+  album: "The Magic Flute", 
+  genre: "Opera"
+})
+
+console.log("songs after pushing", songs );
